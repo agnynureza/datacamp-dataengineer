0c9280c10e786d35d047e76d48c4e7a49b6d4f4d
agny nureza <agny@stockbit.com>
2021-05-25 18:44:51 +0700

    writing function in python chapter 3

---
+++ 5. writing function in python/decorator.py
@@ -0,0 +1,23 @@
+""" Scope """
+bagaimana kita bisa membedakan 
+global
+nonlocal 
+local 
+variable
+
+""" Closure """ 
+variable di simpen dalam closure 
+my_func.__closure__
+my_func.__closure__[0].cell_contents
+
+mengapain ini semua penting ?
+untuk tahap selanjutnya soal decorator
+ 
+1. functions as objects
+2. nested functions 
+3. nonlocal scope
+4. closures
+
+""" "decorator """
+menggunakan @double_args
+gunanya buat manipulate behaviour dari func entah itu input atau output nya 
